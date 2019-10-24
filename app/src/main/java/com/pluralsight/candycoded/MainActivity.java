9b975223ca2d33ac68287f6d7a3b9c14e32461ea
KobbyKnight <22242495+KobbyKnight@users.noreply.github.com>
2019-10-24 22:46:33 +0000

    Revert "Completed module"
    This reverts commit 94070b84c3e560505cfbe07d2e7d2e87981a0cd7.

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -89,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent infoIntent = new Intent(this,InfoActivity.class);
         startActivity(infoIntent);
-        
         return super.onOptionsItemSelected(item);
     }
 
